09db89d08872a14e6c43a17a073c603b846190bc
Fontom's <tomtombert7@gmail.com>
2023-12-06 11:15:36 +0100

    Création du générateur

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main() {
     Sudoku sudoku; // Génération de la grille avec un niveau de complexité de 20
 
     // Affichage de la grille générée
-    std::cout << "Generated grid:" << std::endl;
+    std::cout << "Grille générée :" << std::endl;
     std::cout << sudoku;
 
     // Test des nouvelles fonctions
@@ -20,6 +20,15 @@ int main() {
         std::cout << "Valide." << std::endl;
     } else {
         std::cout << "Invalide." << std::endl;
+    }
+
+    // Test de la génération de grilles Sudoku avec différents niveaux de complexité
+    for (int complexity = 1; complexity <= 5; ++complexity) {
+        Sudoku sudoku(complexity);
+
+        std::cout << "Grille générée avec un niveau de complexité " << complexity << ":" << std::endl;
+        std::cout << sudoku;
+        std::cout << "--------------------------------------------" << std::endl;
     }
 
     return 0;
--- sudoku.cpp
+++ sudoku.cpp
@@ -6,6 +6,36 @@ Sudoku::Sudoku() {
     grid.fill({{0, 0, 0, 0, 0, 0, 0, 0, 0}});
 }
 
+Sudoku::Sudoku(int complexity) : Sudoku() {
+    // Génération de la grille en fonction du niveau de complexité
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<int> dist(1, 9);
+
+    // Le niveau de complexité détermine le nombre de cases pré-remplies
+    int minFilledCells = 81 - 5 * complexity; // Exemple de formule, ajustez selon vos besoins
+    int maxFilledCells = 81 - 4 * complexity; // Exemple de formule, ajustez selon vos besoins
+
+    std::uniform_int_distribution<int> cellsDist(minFilledCells, maxFilledCells);
+
+    int filledCells = cellsDist(gen);
+
+    // Génération des cases pré-remplies
+    for (int i = 0; i < filledCells; ++i) {
+        int row = dist(gen) - 1;
+        int col = dist(gen) - 1;
+        int value = dist(gen);
+
+        // Assurez-vous que la valeur générée est valide dans la ligne, la colonne et le carré
+        if (isValidPlacement(row, col, value)) {
+            grid[row][col] = value;
+        } else {
+            // Si la valeur n'est pas valide, réessayez avec une nouvelle valeur
+            --i;
+        }
+    }
+}
+
 std::ostream& operator<<(std::ostream& os, const Sudoku& sudoku) {
     for (const auto& row : sudoku.grid) {
         for (int value : row) {
